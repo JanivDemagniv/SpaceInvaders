3138795ca572087dda1783c91c732f1493ced589
JanivDemagniv <janivjry@gmail.com>
2024-12-17 22:44:00 +0200

    movement

--- code/main.py
+++ code/main.py
@@ -2,11 +2,13 @@ import pygame
 from os.path import join
 from random import randint
 
+#General Setup
 pygame.init()
 WINDOW_WIDTH,WINDOW_HIEGHT = 1280,720
 display_surface = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HIEGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
+clock = pygame.time.Clock()
 
 #plain surface
 surf = pygame.Surface((100,200))
@@ -16,7 +18,8 @@ x =100
 #Importing an images
 player_surf = pygame.image.load(join('images','player.png')).convert_alpha()
 player_react = player_surf.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HIEGHT / 2))
-player_dir = 1
+player_dir = pygame.math.Vector2(1,1)
+player_speed = 100
 
 meteor_surf = pygame.image.load(join('images','meteor.png')).convert_alpha()
 meteor_react = meteor_surf.get_frect(center = (WINDOW_WIDTH /2,WINDOW_HIEGHT/2 + 100))
@@ -28,6 +31,7 @@ star_surf = pygame.image.load(join('images','star.png')).convert_alpha()
 star_positions = [(randint(0,1280),randint(0,720)) for i in range(20)]
 
 while running:
+    dt = clock.tick() / 1000
     #Event Loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,9 +45,12 @@ while running:
     display_surface.blit(meteor_surf,meteor_react)
     display_surface.blit(laser_surf,laser_rect)
     display_surface.blit(player_surf,player_react)
-    player_react.x += player_dir * 0.1
-    if player_react.right > WINDOW_WIDTH or player_react.left < 0:
-        player_dir *= -1
+    player_react.center += player_dir * player_speed * dt
+    if player_react.bottom >= WINDOW_HIEGHT or player_react.top <= 0:
+        player_dir.y *= -1
+    if player_react.right >= WINDOW_WIDTH or player_react.left <= 0:
+        player_dir.x *= -1
+
     
     pygame.display.update()
 
